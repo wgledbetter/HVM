9fa92059ce1e9e9be78041f8ccfe45c3b8a8a0a4
William G Ledbetter <williamgledbetter@gmail.com>
2024-05-31 14:36:25 -0600

    generate some other input books

--- wgl/01-DemoMain.c
+++ wgl/01-DemoMain.c
@@ -1,7 +1,11 @@
-#include "demos.h"
 #include "hvm.h"
 
+// Books
+// #include "demos.h"
+// #include "input/sum_rec_book.h"
+#include "input/sort_radix_book.h"
+
 int main() {
-  hvm_c((u32*) BOOK_BUF_DEMO1);
+  hvm_c((u32*) BOOK_BUF);
   return 0;
 }
