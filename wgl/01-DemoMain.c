501a74227513345c0d6fb113f2d7e25a7dc836eb
William G Ledbetter <williamgledbetter@gmail.com>
2024-06-01 19:45:19 -0600

    run included hvm examples

--- wgl/01-DemoMain.c
+++ wgl/01-DemoMain.c
@@ -1,11 +1,16 @@
 #include "hvm.h"
 
 // Books
-// #include "demos.h"
+// #include "input/demo1.h"
 // #include "input/sum_rec_book.h"
-#include "input/sort_radix_book.h"
+#include "input/tuples_book.h"
+// #include "input/sort_radix_book.h"
 
 int main() {
+  printf("WGL Main Start\n");
+
   hvm_c((u32*) BOOK_BUF);
+
+  printf("WGL Main End\n");
   return 0;
 }
